5a77af1fb4bf97d1a6117ef9988e1270b05004ad
eles linux <eleswastaken@gmail.com>
2022-08-08 15:47:47 +0000

    Create track model

--- api/tracks/models.py
+++ api/tracks/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+    pass
+
+class Track(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    track_id = models.IntegerField(primary_key=True, unique=True)
+    title = models.CharField(max_length=30)
+    description = models.CharField(blank=True, max_length=300)
+    color = models.CharField(max_length=30)
