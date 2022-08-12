cb3de7062911ec884c40975aedea53e1535bbcf9
eles linux <eleswastaken@gmail.com>
2022-08-12 20:12:30 +0000

    Alter user model
    Add creation and modified dates with save updating them

--- users/models.py
+++ users/models.py
@@ -5,12 +5,24 @@ from django.db import models
 
 class CustomUser(AbstractUser):
 
-    username = models.CharField(max_length=30, unique=True)
+    username    = models.CharField(max_length=30, unique=True)
 
-    password = models.CharField(max_length=100)
-    email = models.EmailField(max_length=100, unique=True)
-    api_key = models.CharField(max_length=100)
+    password    = models.CharField(max_length=100)
+    email       = models.EmailField(max_length=100, unique=True)
+    api_key     = models.CharField(max_length=100)
 
+    created     = models.DateTimeField(editable=False)
+    modified    = models.DateTimeField()
+
+    def save(self, *args, **kwargs):
+        ''' On save, update timestamps '''
+
+        if not self.id:
+            self.created = timezone.now()
+
+        self.modified = timezone.now()
+
+        return super(CustomUser, self).save(*args, **kwargs)
 
     def __str__(self):
         return self.email
